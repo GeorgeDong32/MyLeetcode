c0cc444194fe06ec99961d3215e6e2daa33608f1
George·Dong <georgedong32@foxmail.com>
2023-08-13 16:46:12 +0800

    Q94

--- C#/Leetcode/Program.cs
+++ C#/Leetcode/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Q94*/
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        var result = new List<int>();
+        var stack = new Stack<TreeNode>();
+        var current = root;
+        while (current != null || stack.Count > 0)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.left;
+            }
+            current = stack.Pop();
+            result.Add(current.val);
+            current = current.right;
+        }
+        return result;
+    }
+}

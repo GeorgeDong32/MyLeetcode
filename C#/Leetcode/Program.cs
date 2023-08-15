3691449b92c8d1db147754e79dd7f2fd39ac0aba
George·Dong <georgedong32@foxmail.com>
2023-08-15 09:09:05 +0800

    Q125.1

--- C#/Leetcode/Program.cs
+++ C#/Leetcode/Program.cs
@@ -1,15 +1,25 @@
-﻿/*Q119*/
+﻿/*Q125*/
+using System.Text.RegularExpressions;
+var solution = new Solution();
+Console.WriteLine(solution.IsPalindrome("Marge, let's \"[went].\" I await {news} telegram."));
 
 public class Solution
 {
-    public IList<int> GetRow(int rowIndex)
+    public bool IsPalindrome(string s)
     {
-        var result = new List<int>();
-        result.Add(1);
-        for (var i = 1; i < rowIndex + 1; i++)
+        if (string.IsNullOrEmpty(s))
         {
-            result.Add((int)(1L * result[i - 1] * (rowIndex - i + 1) / i));
+            return true;
         }
-        return result;
+        s = s.Trim();
+        s = Regex.Replace(s, "[ \\[ \\] \\^ \\ \\{ \\} \\-_*×――(^)$%~!@#$…&%￥—+=<>《》!！??？:：•`·、。，；,.;\"‘’“”\'-]", "").ToLower();
+        var left = 0; var right = s.Length - 1;
+        while (left < right)
+        {
+            if (s[left] != s[right])
+                return false;
+            left++; right--;
+        }
+        return true;
     }
 }

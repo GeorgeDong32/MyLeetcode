3fdff26d25df24fe61cba94ad8dcc290c62fa731
George·Dong <georgedong32@foxmail.com>
2023-08-18 09:03:50 +0800

    Q205

--- C#/Leetcode/Program.cs
+++ C#/Leetcode/Program.cs
@@ -1,46 +1,17 @@
-﻿/*Q203*/
+﻿/*Q205*/
 var sol = new Solution();
-
-public class ListNode
-{
-    public int val;
-    public ListNode next;
-    public ListNode(int val = 0, ListNode next = null)
-    {
-        this.val = val;
-        this.next = next;
-    }
-}
 
 public class Solution
 {
-    public ListNode RemoveElements(ListNode head, int val)
+    public bool IsIsomorphic(string s, string t)
     {
-        var current = head;
-        ListNode last = null;
-        while (current != null)
+        var arrs = new List<int>();
+        var arrt = new List<int>();
+        for (int i = 0; i < s.Length; i++)
         {
-            if (current.val == val)
-            {
-                if (current != head)
-                {
-                    last.next = current.next;
-                    current = current.next;
-                    last = last;
-                }
-                else
-                {
-                    head = current.next;
-                    current = current.next;
-                    last = null;
-                }
-            }
-            else
-            {
-                last = current;
-                current = current.next;
-            }
+            arrs.Add(s.IndexOf(s[i]));
+            arrt.Add(t.IndexOf(t[i]));
         }
-        return head;
+        return arrs.SequenceEqual(arrt);
     }
 }

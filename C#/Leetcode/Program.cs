59df7e2bc2b2189bcfea96c003b4d71fab513ec4
George·Dong <georgedong32@foxmail.com>
2023-08-19 15:16:09 +0800

    Q219

--- C#/Leetcode/Program.cs
+++ C#/Leetcode/Program.cs
@@ -1,11 +1,26 @@
-﻿/*Q218*/
+﻿/*Q219*/
 var sol = new Solution();
 
 public class Solution
 {
-    public bool ContainsDuplicate(int[] nums)
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
     {
-        HashSet<int> res = new HashSet<int>(nums);
-        return res.Count != nums.Length;
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (d.ContainsKey(nums[i]) && i - d[nums[i]] <= k)
+            {
+                return true;
+            }
+            if (d.ContainsKey(nums[i]))
+            {
+                d[nums[i]] = i;
+            }
+            else
+            {
+                d.Add(nums[i], i);
+            }
+        }
+        return false;
     }
 }

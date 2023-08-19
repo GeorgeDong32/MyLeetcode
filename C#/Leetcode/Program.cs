70ac07181b4c3cd2f8ce6e64e21b4e1cdc232066
George·Dong <georgedong32@foxmail.com>
2023-08-19 15:08:45 +0800

    Q218

--- C#/Leetcode/Program.cs
+++ C#/Leetcode/Program.cs
@@ -1,31 +1,11 @@
-﻿/*Q206*/
+﻿/*Q218*/
 var sol = new Solution();
-Console.WriteLine(sol.ReverseList(new ListNode(1, new ListNode(2, new ListNode(3, null)))));
-
-public class ListNode
-{
-    public int val;
-    public ListNode next;
-    public ListNode(int val = 0, ListNode next = null)
-    {
-        this.val = val;
-        this.next = next;
-    }
-}
 
 public class Solution
 {
-    public ListNode ReverseList(ListNode head)
+    public bool ContainsDuplicate(int[] nums)
     {
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != null)
-        {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+        HashSet<int> res = new HashSet<int>(nums);
+        return res.Count != nums.Length;
     }
 }

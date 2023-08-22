82c61d29492ff63febc7484c2e195b2fb8c6f1c0
George·Dong <georgedong32@foxmail.com>
2023-08-22 11:53:58 +0800

    Q374

---
+++ C#/easy/374.猜数字大小.cs
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=374 lang=csharp
+ *
+ * [374] 猜数字大小
+ */
+
+// @lc code=start
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution : GuessGame
+{
+    public int GuessNumber(int n)
+    {
+        int l = 1, r = n;
+        while (l < r)
+        {
+            int mid = l + ((r - l) >> 1);
+            if (guess(mid) > 0)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return l;
+    }
+}
+// @lc code=end
+

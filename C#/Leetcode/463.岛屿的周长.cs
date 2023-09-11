07b49797fab2c75b33d622e0dcbb88be9748e5b9
George·Dong <georgedong32@foxmail.com>
2023-09-11 23:31:02 +0800

    Update

---
+++ C#/Leetcode/463.岛屿的周长.cs
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=463 lang=csharp
+ *
+ * [463] 岛屿的周长
+ */
+
+// @lc code=start
+public class Solution
+{
+    public int IslandPerimeter(int[][] grid)
+    {
+        if (grid == null)
+            return 0;
+        int m = grid.Length, n = grid[0].Length;
+        if (m == 0 || n == 0)
+            return 0;
+        int count = 0, near = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == 1)
+                    count++;
+                if (j + 1 < n && grid[i][j + 1] == 1)
+                    near++;
+                if (i + 1 < m && grid[i + 1][j] == 1)
+                    near++;
+            }
+        }
+        return 4 * count - 2 * near;
+    }
+}
+// @lc code=end
+
